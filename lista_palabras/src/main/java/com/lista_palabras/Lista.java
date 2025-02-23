653c905e09ea390fcf64f5199967e6924c3b4bb6
ElidallanaCristancho <cristanchodayana062017@gmail.com>
2025-02-23 18:18:37 -0500

    feat: :construction: se agrego la funcion para ingresar y validar palabras en la clase Lista

--- lista_palabras/src/main/java/com/lista_palabras/Lista.java
+++ lista_palabras/src/main/java/com/lista_palabras/Lista.java
@@ -1,6 +1,29 @@
 package com.lista_palabras;
 
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Lista {
     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+        Set<String> palabras = new TreeSet<>();
+
+        // 1. ingresar palabras
+        System.out.println("Ingrese palabras (escriba 'fin' para terminar):");
+        while (true) {
+            System.out.print("Palabra: ");
+            String palabra = scanner.nextLine();
+            if (palabra.equalsIgnoreCase("fin")) {
+                break;
+            }
+            if (!palabras.add(palabra)) {
+                System.out.println("⚠ La palabra '" + palabra + "' ya está en la lista.");
+            }
+        }
+
+       
+
+        scanner.close();
     }
 }
